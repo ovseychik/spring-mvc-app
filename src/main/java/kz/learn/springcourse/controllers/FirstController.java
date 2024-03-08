d3f61496d8e987d43a0fdad672f2a6ac9da50fc2
ovseychiks <ovseychiks@kacd.kz>
2024-03-08 20:22:40 +0500

    Model

--- src/main/java/kz/learn/springcourse/controllers/FirstController.java
+++ src/main/java/kz/learn/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package kz.learn.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,9 +13,10 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println("Hello, " + name + " " + surname);
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 
+        model.addAttribute("message", "Hello, " + name + " " + surname);
         return "first/hello";
     }
 
