8253244449435637fce4fc18ad40ed81b0ab9fb8
ovseychiks <ovseychiks@kacd.kz>
2024-03-25 18:51:15 +0500

    Dev 6 CRUD operations: PATCH and DELETE

--- src/main/java/kz/learn/springcourse/controllers/PeopleController.java
+++ src/main/java/kz/learn/springcourse/controllers/PeopleController.java
@@ -13,6 +13,7 @@ public class PeopleController {
 
     private final PersonDao personDao;
 
+    @Autowired
     public PeopleController(PersonDao personDao) {
         this.personDao = personDao;
     }
@@ -37,6 +38,24 @@ public class PeopleController {
     @PostMapping
     public String create(@ModelAttribute("person") Person person) {
         personDao.save(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("person", personDao.show(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+        personDao.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        personDao.delete(id);
         return "redirect:/people";
     }
 }
